623d530c7756cd672edf0c105a5cddb33872d033
Charité Igirimbabazi <62570559+lord-charite@users.noreply.github.com>
2024-06-16 13:14:48 -0400

    Create sort.h

---
+++ sort.h
@@ -0,0 +1,15 @@
+/*
+ * sort.h
+ * Header file for sort function prototypes.
+ */
+
+#ifndef SORT_H
+#define SORT_H
+
+#include "student.h"
+
+void sortAverage(int *N, int *sort_array, student_t *studentdata);
+void sortFirstname(int *N, int *sort_array, student_t *studentdata);
+void sort(int *N, int *sort_array, student_t *studentdata);
+
+#endif // SORT_H
