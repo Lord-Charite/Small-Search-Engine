7526b94e95f159a2212adbcb339069bb2876f116
Charité Igirimbabazi <62570559+lord-charite@users.noreply.github.com>
2024-06-16 13:13:32 -0400

    Create search.h

---
+++ search.h
@@ -0,0 +1,15 @@
+/*
+ * search.h
+ * Header file for search function prototypes.
+ */
+
+#ifndef SEARCH_H
+#define SEARCH_H
+
+#include "student.h"
+
+void searchAverage(int *N, int *sort_studentdata, student_t *studentdata);
+void searchTest1(int *N, int *sort_studentdata, student_t *studentdata);
+void search(int *N, int *sort_studentdata, student_t *studentdata);
+
+#endif // SEARCH_H
